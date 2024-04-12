149094c97b7289323f466b39a9123994d386ff04
Ignacio Cuello <icuelloreyes97@gmail.com>
2024-04-12 21:57:06 +1000

    Register template/finalize poll command

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -1,10 +1,20 @@
-Bot = Discordrb::Commands::CommandBot.new(
+Bot = Discordrb::Bot.new(
   token: Rails.application.credentials.dig(:discord, :token),
   client_id: Rails.application.credentials.dig(:discord, :app_id),
-  prefix: '/'
+  intents: [:server_messages]
 )
 
-puts "invite_url: #{Bot.invite_url}"
+# puts "invite_url: #{Bot.invite_url}"
+#
+Bot.register_application_command(
+  :template,
+  'Get template messages',
+  server_id: Rails.application.credentials.dig(:discord, :server_id)
+) do |cmd|
+  cmd.subcommand('finalize poll', 'Get template message for finalizing a poll') do |sub|
+    sub.string(sutta_id)
+  end
+end
 
 Bot.message(content: 'Hello') do |event|
   event.respond 'Hello, World!'
