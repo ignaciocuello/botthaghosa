f5dd1107e9d8f6c8e6539cdbaf84cc4e8a2a5efc
Ignacio Cuello <icuelloreyes97@gmail.com>
2024-04-12 22:31:55 +1000

    Respond to message with template for poll

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -11,13 +11,21 @@ Bot.register_application_command(
   'Get template messages',
   server_id: Rails.application.credentials.dig(:discord, :server_id)
 ) do |cmd|
-  cmd.subcommand('finalize poll', 'Get template message for finalizing a poll') do |sub|
-    sub.string(sutta_id)
+  cmd.subcommand('poll', 'Get template message for finalizing a poll') do |sub|
+    sub.string('suttaid', 'The ID of the sutta that won the poll', required: true)
   end
 end
 
-Bot.message(content: 'Hello') do |event|
-  event.respond 'Hello, World!'
+# TODO: add date using ice cube
+Bot.application_command(:template).subcommand('poll') do |event|
+  template = <<~TEMPLATE
+    #{event.options['suttaid']} had the most votes, so we will be studying it in our next sutta discussion on {date}.
+
+    Don’t worry if your chosen sutta didn’t make it, we will put up the 2nd and 3rd most voted in the next poll.
+
+    Thanks to everyone that cast their vote. 🙏🙏🙏
+  TEMPLATE
+  event.respond(content: template, ephemeral: true)
 end
 
 background = true
