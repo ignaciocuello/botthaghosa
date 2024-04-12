f8e32760e8b2e6e5f413f164294146c5082d72c9
Ignacio Cuello <icuelloreyes97@gmail.com>
2024-04-12 23:17:55 +1000

    Format discussion date
    Doing this in service for now, really should be in a helper, but
    will clean it up once we get a more sophisticated setup.

--- app/services/discussion_date.rb
+++ app/services/discussion_date.rb
@@ -3,14 +3,22 @@ class DiscussionDate
 
   class << self
     def first(day_name)
-      discussion_schedule(day_name).next_occurrence
+      date = discussion_schedule(day_name).next_occurrence
+      format(date)
     end
 
     def second(day_name)
-      discussion_schedule(day_name).next_occurrence + 1.week
+      date = discussion_schedule(day_name).next_occurrence + 1.week
+      format(date)
     end
 
     private
+
+    # TODO: Break this out to a helper, and don't worry about formatting for this class
+    # Unsure how to include helpers in the initializer.
+    def format(date)
+      date.strftime('%B %-d')
+    end
 
     def discussion_schedule(day_name)
       schedule = IceCube::Schedule.new(START_DATE) do |s|
--- test/services/discussion_date_test.rb
+++ test/services/discussion_date_test.rb
@@ -4,25 +4,25 @@ require 'test_helper'
 class DiscussionDateTest < ActiveSupport::TestCase
   test 'second saturday' do
     travel_to '12-04-2024'.to_date do
-      assert_equal '20-04-2024'.to_date, DiscussionDate.second(:saturday).to_date
+      assert_equal 'April 20', DiscussionDate.second(:saturday)
     end
   end
 
   test 'first monday' do
     travel_to '12-04-2024'.to_date do
-      assert_equal '22-04-2024'.to_date, DiscussionDate.first(:monday).to_date
+      assert_equal 'April 22'.to_date, DiscussionDate.first(:monday).to_date
     end
   end
 
   test 'second wednesday' do
     travel_to '12-04-2024'.to_date do
-      assert_equal '01-05-2024'.to_date, DiscussionDate.second(:wednesday).to_date
+      assert_equal 'May 1'.to_date, DiscussionDate.second(:wednesday).to_date
     end
   end
 
   test 'first thursday' do
     travel_to '12-04-2024'.to_date do
-      assert_equal '25-04-2024'.to_date, DiscussionDate.first(:thursday).to_date
+      assert_equal 'April 25'.to_date, DiscussionDate.first(:thursday).to_date
     end
   end
 end
