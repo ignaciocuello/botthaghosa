cc2261ce8c475ed573ccef578296e61bebc01134
Ignacio Cuello <icuelloreyes97@gmail.com>
2024-04-12 23:40:08 +1000

    Add notify community template

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -16,22 +16,44 @@ Bot.register_application_command(
       sub.string(:sutta_id, 'The ID of the sutta that won the poll', required: true)
     end
   end
+
+  cmd.subcommand_group(:notify, 'Get template messages for notifying') do |group|
+    group.subcommand(:community, 'Get template message for notifying the community') do |sub|
+      sub.string(:sutta_id, 'The ID of the sutta that we will be discussing', required: true)
+    end
+  end
 end
 
 Bot.application_command(:template).group(:poll) do |group|
   group.subcommand(:finalize) do |event|
     Time.use_zone('Australia/Melbourne') do
       discussion_date = DiscussionDate.second(:saturday)
-
+      sutta_id = event.options['sutta_id']
       template = <<~TEMPLATE
-        #{event.options['sutta_id']} had the most votes, so we will be studying it in our next sutta discussion on #{discussion_date}.
+        #{sutta_id} had the most votes, so we will be studying it in our next sutta discussion on #{discussion_date}.
 
         Don’t worry if your chosen sutta didn’t make it, we will put up the 2nd and 3rd most voted in the next poll.
 
         Thanks to everyone that cast their vote. 🙏🙏🙏
       TEMPLATE
+
       event.respond(content: template, ephemeral: true)
     end
+  end
+end
+
+Bot.application_command(:template).group(:notify) do |group|
+  group.subcommand(:community) do |event|
+    sutta_id = event.options['sutta_id']
+    template = <<~TEMPLATE
+      Hey everyone! :wave:
+
+      Just a quick heads up about our sutta discussion this **Saturday at 7PM** on **#{sutta_id}**. It's a great opportunity to dive into some deep Buddhist teachings and share your thoughts.
+
+      Join us on Zoom [here](https://us06web.zoom.us/j/84146622864?pwd=fvhLV0ZF7FxUdXzCWi8JsVOtPh8U7u.1). Hope to see you there for a meaningful and engaging conversation!
+    TEMPLATE
+
+    event.respond(content: template, ephemeral: true)
   end
 end
 
