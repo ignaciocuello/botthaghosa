17a19ae7bec526e46866a15dc3ce8de92bfdf6fd
Ignacio Cuello <icuelloreyes97@gmail.com>
2024-04-21 18:32:11 +1000

    Do not load bot in tests

--- config/initializers/botthaghosa.rb
+++ config/initializers/botthaghosa.rb
@@ -1,5 +1,7 @@
-ActiveSupport::Reloader.to_prepare do
-  bot = Botthaghosa.new
-  bot.setup_commands
-  bot.run
+unless Rails.env.test?
+  ActiveSupport::Reloader.to_prepare do
+    bot = Botthaghosa.new
+    bot.setup_commands
+    bot.run
+  end
 end
