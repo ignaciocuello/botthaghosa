ad57e71105eac23e394bcff7e284a93c7a6beac9
Ignacio Cuello <icuelloreyes97@gmail.com>
2024-04-24 21:39:35 +1000

    Simplify commands test

--- test/services/commands_test.rb
+++ test/services/commands_test.rb
@@ -13,21 +13,7 @@ class CommandsTest < ActiveSupport::TestCase
           title: 'The Root of All Things'
         )
 
-        expected = <<~TEMPLATE
-          I have noted the sutta for our next discussion on May 04 as "MN 1 - The Root of All Things".
-
-          Here is a message that you can use to notify the community in #announcements (just click on the copy button on the top right):
-
-          ```
-          MN 1 had the most votes, so we will be studying it in our next sutta discussion on May 04.
-
-          Don’t worry if your chosen sutta didn’t make it, we will put up unvoted suttas in subsequent polls.
-
-          Thanks to everyone that cast their vote. 🙏🙏🙏
-          ```
-        TEMPLATE
-        assert_equal 'MN 1', DiscussionSession.last.sutta.abbreviation
-        assert_equal expected, output
+        assert_includes output, 'MN 1 - The Root of All Things'
       end
     end
   end
@@ -42,12 +28,9 @@ class CommandsTest < ActiveSupport::TestCase
             link: 'https://www.google.com'
           )
 
-          expected = <<~TEMPLATE
-            Thanks! I have noted the discussion document for our next discussion on May 04 as [04-05-24 MN 1](https://www.google.com).
-          TEMPLATE
+          assert_includes output, '[04-05-24 MN 1](https://www.google.com)'
           assert_equal 'https://www.google.com', DiscussionSession.last.document.link
           assert_equal '04-05-24 MN 1', DiscussionSession.last.document.title
-          assert_equal expected, output
         end
       end
     end
@@ -55,33 +38,12 @@ class CommandsTest < ActiveSupport::TestCase
 
   test 'output notify community template' do
     create_session
-
-    output = Commands.discussion_template_notify_community
-    expected = <<~TEMPLATE
-      ```
-      Hey everyone! :wave:
-
-      Just a quick heads up about our sutta discussion this **Saturday at 7PM** on **MN 1 - The Root of All Things**. It's a great opportunity to dive into some deep Buddhist teachings and share your thoughts.
-
-      Join us on Zoom [here](#{Rails.application.credentials.dig(:zoom, :session_link)}). Hope to see you there for a meaningful and engaging conversation!
-      ```
-    TEMPLATE
+    assert_includes Commands.discussion_template_notify_community, 'Join us on Zoom'
   end
 
   test 'output template document share' do
     create_session(with_document: true)
-
-    output = Commands.discussion_template_document_share
-    expected = <<~TEMPLATE
-      ```
-      Hey everyone! :wave: Here’s the link to the session document for our upcoming sutta discussion happening this Saturday at 7PM! :clock7:#{' '}
-
-      - [MN 1 - The Root of All Things](https://www.google.com)
-
-      Looking forward to seeing you there!
-      ```
-    TEMPLATE
-    assert_equal expected, output
+    assert_includes Commands.discussion_template_document_share, 'Looking forward to seeing you there!'
   end
 
   teardown do
